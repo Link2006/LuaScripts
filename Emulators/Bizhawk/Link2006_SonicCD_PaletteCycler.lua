0acd40588739c73de9e4dfe1d952ea9ca8554f2f
Link2006 <984642+Link2006@users.noreply.github.com>
2026-04-18 08:19:42 -0400

    Fixed comments in PaletteCycler

--- Emulators/Bizhawk/Link2006_SonicCD_PaletteCycler.lua
+++ Emulators/Bizhawk/Link2006_SonicCD_PaletteCycler.lua
@@ -13,10 +13,11 @@ while true do
 	for key,addr in pairs(AddrTbl) do --Cycle through the addresses we have 
 		--Copy the current vars into a table to cycle them afterwards.
 		local Mem = {memory.read_u16_be(addr),memory.read_u16_be(addr+2),memory.read_u16_be(addr+4)}
-		--write them in the order of 3 -> 1; 1 -> 2, 2 -> 3
-		memory.write_u16_be(addr,Mem[2]) --Targetaddr, 3rd value
-		memory.write_u16_be(addr+2,Mem[3]) --TargetAddr + Offset of 2 bytes, 1st Value
-		memory.write_u16_be(addr+4,Mem[1]) --TargetAddr + Offset of 4 bytes, 2nd Value
+		--Take the 2nd value, write it in place of the first
+		--Repeat that for 3->2 and 1->3
+		memory.write_u16_be(addr,Mem[2]) 
+		memory.write_u16_be(addr+2,Mem[3])
+		memory.write_u16_be(addr+4,Mem[1])
 	end 
 	for i=1,6 do --advance 6 frames
 		emu.frameadvance();
